3a2b36a1d803cb16b4756b301b215a80c8f0c193
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-17 18:27:58 +0530

    [gurpreet] UC2_CreateEmployeeTable

--- SQLEmployeePayRollProblem/SQLEmployeePayRollProblem/SQLQuery1.sql
+++ SQLEmployeePayRollProblem/SQLEmployeePayRollProblem/SQLQuery1.sql
@@ -1,2 +1,10 @@
 -------UC1-------
 CREATE DATABASE PAYROLL_SERVICE
+
+-------UC2-------
+CREATE TABLE Employee_Payroll(
+Id int identity(1,1) PRIMARY KEY,--first 1=it should be start with first row and second indicate 1=increment by 1---
+Name varchar(200),
+Salary float,
+StartDate datetime
+);
