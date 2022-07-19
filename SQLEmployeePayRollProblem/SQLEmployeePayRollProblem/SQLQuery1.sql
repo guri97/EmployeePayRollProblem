9d85b99393229f917bfd287fdc9b460525575375
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-19 21:38:41 +0530

    [gurpreet] UC9_AlteringTheTablesByAddingColumns

--- SQLEmployeePayRollProblem/SQLEmployeePayRollProblem/SQLQuery1.sql
+++ SQLEmployeePayRollProblem/SQLEmployeePayRollProblem/SQLQuery1.sql
@@ -66,3 +66,5 @@ select * from employee_payroll;
 
 ALTER TABLE employee_payroll
 ADD Address varchar(200) default 'Bengaluru';
+-------------UC9 Extend the employee table----------------
+ALTER TABLE employee_payroll ADD BasicSalary float,Deductions float,TaxablePay float,IncomeTax float,NetPay float;
